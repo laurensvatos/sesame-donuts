7d7fba38fd6bf9aa6bb75e9ab7b32d46fea763c3
Lauren <lsvatos@pdx.edu>
2022-11-30 09:12:29 -0800

    mobile homepage addition

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-//hellllllo
+function toggleIcon() {
+    $(".icon").on("click", function() {
+        $(".icon").toggleClass("active")
+        $(".navigation-mobile").slideToggle(300)
+    })
+}
+
+function closeMobileMenu() {
+    $(".navigation-mobile").on("click", "a", function() {
+        $(".icon").trigger("click")
+    })
+}
+
+
+//when the page loads call toggleIcon;
+$(toggleIcon)
+$(closeMobileMenu)
